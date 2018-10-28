a5393659635393e0af7c7781b8b686e82aa4d6d3
Martin Iotchev <m.iotchev@gmail.com>
2018-10-28 10:33:54 -0700

    Initial commit

---
+++ src/components/Index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./index.scss";
+
+const Index = () => {
+    return <div className={style.index}>React!</div>
+};
+
+export default Index;
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HTMLWebPackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HTMLWebPackPlugin({
+    template: './src/index.html',
+    filename: './index.html'
+});
+
+module.exports = {
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                            importLoaders: 1,
+                            localIdentName: "[name]_[local]_[hash:base64]",
+                            sourceMap: true,
+                            minimize: true
+                        }
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [htmlPlugin]
+
+};
